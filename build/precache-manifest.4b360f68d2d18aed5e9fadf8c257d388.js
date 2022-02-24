8621ead64cbdcbff189e4432a116e8b99764bea1
kmothibe <kmothibe@student.wethinkcode.co.za>
2022-02-24 17:54:40 +0200

    d

---
+++ build/precache-manifest.4b360f68d2d18aed5e9fadf8c257d388.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3872aca4c5cdb3690c18",
+    "url": "/Jackie/static/css/main.4fda041d.chunk.css"
+  },
+  {
+    "revision": "3872aca4c5cdb3690c18",
+    "url": "/Jackie/static/js/main.3872aca4.chunk.js"
+  },
+  {
+    "revision": "0eaad9b19e0a4b4c07e3",
+    "url": "/Jackie/static/css/1.260f8475.chunk.css"
+  },
+  {
+    "revision": "0eaad9b19e0a4b4c07e3",
+    "url": "/Jackie/static/js/1.0eaad9b1.chunk.js"
+  },
+  {
+    "revision": "41b28daf97d3b535a360",
+    "url": "/Jackie/static/js/runtime~main.41b28daf.js"
+  },
+  {
+    "revision": "5da9735e9d338d2cae797938a2da940d",
+    "url": "/Jackie/static/media/conf.5da9735e.JPG"
+  },
+  {
+    "revision": "d1c555e98f7dc6963fb7be8e848cb4e9",
+    "url": "/Jackie/static/media/kids.d1c555e9.jpg"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Jackie/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Jackie/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Jackie/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Jackie/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Jackie/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "9e582ebcc02e906bf3ddec99162455f0",
+    "url": "/Jackie/index.html"
+  }
+];
